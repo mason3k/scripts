f79bf419d8876d045e490712e642a12eaa436cce
Sydney Jones <sydney.u.jones@gmail.com>
2023-02-19 14:55:02 -0600

    refactor: :recycle: DRY,  refactor

--- calendar_events_mac.py
+++ calendar_events_mac.py
@@ -1,9 +1,10 @@
 import csv
-from datetime import date, datetime, timedelta, timezone
+from datetime import date, datetime, time
 from typing import TextIO
-
+from pathlib import Path
 import dateutil.parser
 import pyperclip
+from dataclasses import dataclass
 
 TODAY = date.today()
 FILTERED_SUBJECTS = [
@@ -16,27 +17,64 @@ FILTERED_SUBJECTS = [
 ]
 
 
+@dataclass
+class Event:
+    title: str
+    start_time_str: str
+    end_time_str: str
+
+    def __post_init__(self):
+        self.start_time = dateutil.parser.isoparse(self.start_time_str)
+        self.end_time = dateutil.parser.isoparse(self.end_time_str)
+
+    def __bool__(self) -> bool:
+        return self.title in FILTERED_SUBJECTS
+
+    @property
+    def is_today(self) -> bool:
+        return self.start_time.date() == TODAY
+
+
 def main():
-    filename = TODAY.strftime("%Y_%m_%d") + ".txt"
     output = ""
     with (
-        open(r"/Users/sjones/Downloads/calendar.csv", encoding="utf-8") as csv_file,
-        open(f"/Users/sjones/Downloads/daily_tasks/{filename}", "w") as output_file,
+        Path(r"/Users/sjones/Downloads/calendar.csv").open(encoding="utf-8") as csv_file,
+        Path(f"/Users/sjones/Downloads/daily_tasks/{TODAY.strftime('%Y_%m_%d')}.txt").open(
+            "w"
+        ) as output_file,
     ):
         csv_reader = csv.DictReader(csv_file, delimiter=",")
 
-        todays_events = list(filter(good_row, csv_reader))
-        handle_lunch(todays_events)
-        todays_events.sort(key=lambda one_row: one_row["Start Time"])
+        todays_events = []
+        no_lunch = False
+        for row in csv_reader:
+            event = Event(row["Event Title"], row["Start Time"], row["End Time"])
+            if event and event.is_today:
+                todays_events.append(event)
+                if event.title == "Lunch" or event.start_time.hour == 11:
+                    no_lunch = True
+                if event.title == "Lunch":
+                    event.title = "Matt Lunch"
 
-        for row in todays_events:
-            if row["Start Time"].hour > 17:
+        if not no_lunch:
+            todays_events.append(
+                Event(
+                    "Lunch",
+                    str(datetime.combine(TODAY, time(hour=11))),
+                    str(datetime.combine(TODAY, time(hour=11, minute=30))),
+                )
+            )
+
+        todays_events.sort(key=lambda event: event.start_time)
+
+        for event in todays_events:
+            if event.start_time.hour > 17:
                 break
-            start_time = datetime.strftime(row["Start Time"], "%H:%M")
-            end_time = datetime.strftime(row["End Time"], "%H:%M")
-            subject = row["Event Title"]
             output = write_out(
-                f"- [ ] {start_time} {subject} \n- [ ] {end_time}", output_file, output
+                f"- [ ] {datetime.strftime(event.start_time, '%H:%M')} {event.title} "
+                f"\n- [ ] {datetime.strftime(event.end_time, '%H:%M')}",
+                output_file,
+                output,
             )
 
         write_out("- [ ] 17:00 BREAK", output_file, output)
@@ -44,66 +82,11 @@ def main():
         pyperclip.copy(output)
 
 
-def write_out(text: str, output_file: TextIO, running_output: str):
+def write_out(text: str, output_file: TextIO, all_lines: str):
     print(text)
     output_file.write(f"\ntext")
-    running_output += f"\n{text}"
-    return running_output
-
-
-def handle_lunch(todays_events):
-    no_lunch = False
-    for row in todays_events:
-        if row["Event Title"] == "Lunch":
-            no_lunch = True
-            row["Event Title"] = "Matt Lunch"
-        if row["Start Time"].hour == 11:
-            no_lunch = True
-    if not no_lunch:
-        todays_events.append(
-            {
-                "Event Title": "Lunch",
-                "Start Time": datetime(
-                    day=TODAY.day,
-                    month=TODAY.month,
-                    year=TODAY.year,
-                    hour=11,
-                    minute=0,
-                    tzinfo=timezone(-timedelta(hours=6)),
-                ),
-                "End Time": datetime(
-                    day=TODAY.day,
-                    month=TODAY.month,
-                    year=TODAY.year,
-                    hour=11,
-                    minute=30,
-                    tzinfo=timezone(-timedelta(hours=6)),
-                ),
-            }
-        )
-
-
-def good_row(row):
-    return _row_is_today(row) and _not_filtered_subject(row)
-
-
-def _convert_to_datetime(row, key):
-    string_time = row[key]
-    row[key] = dateutil.parser.isoparse(string_time)
-
-
-def _not_filtered_subject(row):
-    try:
-        return row["Event Title"] not in FILTERED_SUBJECTS
-    except KeyError:
-        return True
-
-
-def _row_is_today(row):
-    for key in ("Start Time", "End Time"):
-        _convert_to_datetime(row, key)
-    event_date = row["Start Time"].date()
-    return event_date == TODAY
+    all_lines += f"\n{text}"
+    return all_lines
 
 
 if __name__ == "__main__":
